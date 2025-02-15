fab269a8a61eaa9893be73a15fcab3bc1e01cc30
Aswin Viswajith N R <103254420+AswinViswajith@users.noreply.github.com>
2025-02-15 10:53:25 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (13.48%) - LeetSync

---
+++ 2507-number-of-common-factors/number-of-common-factors.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int commonFactors(int a, int b) {
+        int limit = min(a,b);
+        int count = 0;
+        for(int i=1;i<=limit;i++)
+        {
+            if(a%i ==0 && b%i ==0)
+            {
+                count+=1;
+            }
+        }
+        return count;
+    }
+};
