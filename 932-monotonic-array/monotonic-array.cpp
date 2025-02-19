bfe12cfe9decce4d2b725e0920fbba588af997e4
Aswin Viswajith N R <103254420+AswinViswajith@users.noreply.github.com>
2025-02-19 14:44:06 +0530

    Time: 7 ms (10.14%) | Memory: 100.4 MB (65.30%) - LeetSync

--- 932-monotonic-array/monotonic-array.cpp
+++ 932-monotonic-array/monotonic-array.cpp
@@ -5,22 +5,20 @@ public:
         int asc=0,des=0;
         for(int i=0;i<n-1;i++)
         {
-            if(nums[i] <= nums[i+1])
+            if(nums[i] < nums[i+1])
             {
-                asc += 1;
+                asc = 1;
+            }
+            if(nums[i] > nums[i+1])
+            {
+                des = 1;
             }
         }
-        for(int i=0;i<n-1;i++)
+        
+        if(asc == 1 && des == 1)
         {
-            if(nums[i] >= nums[i+1])
-            {
-                des += 1;
-            }
+            return false;
         }
-        if(asc == n-1 || des == n-1)
-        {
-            return true;
-        }
-        return false;
+        return true;
     }
 };
