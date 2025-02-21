10605ec977b35884e3174f7ea8adc11f1812a5ac
Aswin Viswajith N R <103254420+AswinViswajith@users.noreply.github.com>
2025-02-21 11:16:23 +0530

    Time: 5 ms (32.97%) | Memory: 9.6 MB (90.74%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        int m = s.length();
+        int n = t.length();
+        if(n != m)
+        {
+            return false;
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]!=t[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
